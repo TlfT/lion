567ad74ecb3df5634f2b8057d8e0d64dff3b34cb
lfl <mclr@ymail.com>
2013-08-30 01:25:12 -0700

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lion::Application.config.secret_key_base = '591187efe151bd6934934755d0a54c2eb8e59df3effc1534698c59a1e93094d634a1fee19a138acc30846b8d0f5ec0836a11b3d42949edaca7bb2dde7ecb09bf'
